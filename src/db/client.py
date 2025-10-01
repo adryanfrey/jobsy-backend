3585d2429ed088d67d867bc11cf9aefd028e667d
Adryan Frey <adryanfrey886@gmail.com>
2025-10-01 17:50:03 -0300

    supabase connection

---
+++ src/db/client.py
@@ -0,0 +1,15 @@
+import os
+from supabase import create_client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+if not url or not key:
+    raise ValueError(
+        "SUPABASE_URL and SUPABASE_KEY must be set in environment variables"
+    )
+
+supabase_client = create_client(url, key)
